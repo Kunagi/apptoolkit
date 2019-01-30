1ed27ec0207b6224ae2b699033cdc91b4bc92d9b
Witoslaw Koczewski <wi@koczewski.de>
2019-01-30 17:29:50 +0100

    http-server: configurable port and app-js

--- src/apptoolkit/mod/http_server.clj
+++ src/apptoolkit/mod/http_server.clj
@@ -8,26 +8,27 @@
 
    [appkernel.api :as appkernel]))
 
+(def dev-port 3000)
+(def dev-app-js-path "cljs-out/dev-main.js")
 
-(def port 3000)
+(defn app-html [app-js-path]
+  (str
+   "<!DOCTYPE html>
+   <html>
+   <head>
+     <meta charset=\"UTF-8\">
+     <meta name=\"viewport\" content=\"minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no\">
+     <link rel=\"icon\" href=\"https://clojurescript.org/images/cljs-logo-icon-32.png\">
+   </head>
+   <body>
+     <div id=\"app\">Loading...</div>
+     <script src=\"" app-js-path "\" type=\"text/javascript\"></script>
+     <script>apptoolkit.browserapp.api.start();</script>
+   </body>
+   </html>"))
 
-(def home
-  "<!DOCTYPE html>
-  <html>
-  <head>
-    <meta charset=\"UTF-8\">
-    <meta name=\"viewport\" content=\"minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no\">
-    <link rel=\"icon\" href=\"https://clojurescript.org/images/cljs-logo-icon-32.png\">
-  </head>
-  <body>
-    <div id=\"app\">Loading...</div>
-    <script src=\"/cljs-out/dev-main.js\" type=\"text/javascript\"></script>
-    <script>apptoolkit.browserapp.api.start();</script>
-  </body>
-  </html>")
-
-(def default-routes
-  [(compojure/GET  "/" [] home)
+(defn default-routes [app-js-path]
+  [(compojure/GET  "/" [] (app-html app-js-path))
    (compojure-route/files "/" {:root "target/public"})
    (compojure-route/not-found "404 - Page not found")])
 
@@ -44,9 +45,11 @@
 (defn start!
   [db]
   (let [;; routes-from-modules [] (app/extensions-get-concatinated db :http-server/routes :routes)
+        app-js-path (get db :http-server/app-js-path dev-app-js-path)
+        port (get db :http-server/port dev-port)
         routes-from-modules (appkernel/execute-query-sync-and-merge-results db [:http-server/routes])
         plain-routes (into [] routes-from-modules)
-        plain-routes (into plain-routes default-routes)]
+        plain-routes (into plain-routes (default-routes app-js-path))]
     (http-kit/run-server (app-routes plain-routes) {:port port}))
     ;; (app/log :debug ::started {:port port}))
   db)
