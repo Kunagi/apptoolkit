6cbc3f045203dcf040331da63974de0147d3f323
Witoslaw Koczewski <wi@koczewski.de>
2019-02-06 21:40:38 +0100

    preloader

--- src/apptoolkit/http_server/mod.clj
+++ src/apptoolkit/http_server/mod.clj
@@ -13,6 +13,22 @@
 
 (def dev-port 3000)
 
+(def preloader-css
+  ".preloader {margin: 100px auto 0; width: 66px; height: 12px;}
+div.preloader div {color: #000; margin: 5px 0; text-transform: uppercase; font-family: 'Arial', sans-serif; font-size: 9px; letter-spacing: 2px;}
+.preloader .line {width: 1px; height: 12px; background: #000; margin: 0 1px; display: inline-block; animation: opacity-1 1000ms infinite ease-in-out;}
+.preloader .line-1 { animation-delay: 800ms; }
+.preloader .line-2 { animation-delay: 600ms; }
+.preloader .line-3 { animation-delay: 400ms; }
+.preloader .line-4 { animation-delay: 200ms; }
+.preloader .line-6 { animation-delay: 200ms; }
+.preloader .line-7 { animation-delay: 400ms; }
+.preloader .line-8 { animation-delay: 600ms; }
+@keyframes opacity-1 {0% {opacity: 1;} 50% {opacity: 0;} 100% {opacity: 1;}}
+@keyframes opacity-2 {0% {opacity: 1; height: 15px;} 50% {opacity: 0; height: 12px;} 100% {opacity: 1; height: 15px;}}")
+
+(def preloader-html
+  "<div class='preloader'> <div>Loading</div> <span class='line line-1'></span> <span class='line line-2'></span> <span class='line line-3'></span> <span class='line line-4'></span> <span class='line line-5'></span> <span class='line line-6'></span> <span class='line line-7'></span> <span class='line line-8'></span> </div>")
 
 (defn app-html [page-config]
   (str
@@ -21,10 +37,15 @@
    <head>
      <meta charset=\"UTF-8\">
      <meta name=\"viewport\" content=\"minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no\">
-     <link rel=\"icon\" href=\"https://clojurescript.org/images/cljs-logo-icon-32.png\">
-   </head>
+     <link rel=\"icon\" href=\"https://clojurescript.org/images/cljs-logo-icon-32.png\">"
+   "<style>"
+   preloader-css
+   "</style>"
+   " </head>
    <body>
-     <div id=\"app\">Loading...</div>
+     <div id=\"app\">"
+   preloader-html
+   "</div>
      <script src=\"" (:app-js-path page-config) nil "\"\"></script>
      <script>apptoolkit.browserapp.api.start();</script>
    </body>
